15c39adf9dd79b496f1ab2bb24980f73276e6864
Anh Pham <nhattungmedia.com@gmail.com>
2022-03-10 14:34:03 +0700

    ok

---
+++ api/index.js
@@ -0,0 +1,22 @@
+const posts = [
+  {
+    id: 1,
+    name: "Nextjs is awesome",
+  },
+  {
+    id: 2,
+    name: "Using TypeScript with Nextjs",
+  },
+  {
+    id: 3,
+    name: "GraphQL Vs REST",
+  },
+  {
+    id: 4,
+    name: "Bridging in React Native",
+  },
+];
+export default (req, res) => {
+  res.statusCode = 200;
+  res.json(posts);
+};
