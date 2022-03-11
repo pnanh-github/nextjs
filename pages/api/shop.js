4969ca5cd0577e543f2617ce7dc1d7d2a911d316
Anh Pham <nhattungmedia.com@gmail.com>
2022-03-11 22:53:06 +0700

    update shop

--- pages/api/shop.js
+++ pages/api/shop.js
@@ -20,6 +20,13 @@ const posts = [
     vnd: 2600000,
     image: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcThvr4RnRBirJyhuu5IFUDVItmMfSYB3dpCNAm5k52vjY7nuUYP3IKiuXsC--sQZn6TzmFzeB3lOw&usqp=CAc",
   },
+  {
+    id: "4",
+    name: "True Wireless Edifier X3s",
+    price: 58,
+    vnd: 609000,
+    image: "http://shop.loa.com.vn/media/catalog/product/cache/1/thumbnail/1000x750/0dc2d03fe217f8c83829496872af24a0/x/3/x3s-3.jpg",
+  },
 ];
 export default (req, res) => {
   res.statusCode = 200;
