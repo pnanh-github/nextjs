f4a8c0b31c6352a27f94aabf4a4a2d860081facf
Anh Pham <nhattungmedia.com@gmail.com>
2022-03-26 17:09:13 +0700

    done

--- pages/api/sendNotification/index.js
+++ pages/api/sendNotification/index.js
@@ -8,9 +8,32 @@ const sendNotification = (req, res, next) => {
       return res.status(400).json({ error: err.message });
     } else {
       res.statusCode = 200;
+      let result = rows.map((a) => a.token);
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: "key=AAAApJtIZOc:APA91bHmH5HjVTjU8X7xgCaVwdFWXyDqh_MF9xIrxjyPslqBrEanrNGrFQCN82aIHwLfrNCfR4fZArttfgoh3W5CMF0areL7dNhNgUxSU97j2rOg1yHcdk4PnmSdO-Zzr6rpYQIGFpq2",
+        "Cache-Control": "no-cache",
+      };
+      const postData = {
+        registration_ids: result,
+        priority: "high",
+        notification: {
+          title: req.body.title,
+        },
+      };
+      axios
+        .post("https://fcm.googleapis.com/fcm/send", postData, {
+          headers: headers,
+        })
+        .then((res) => {
+          console.log(`statusCode: ${res.status}`);
+          console.log(res);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
       return res.json({
-        message: "success",
-        data: rows,
+        message: "sent",
       });
     }
   });
